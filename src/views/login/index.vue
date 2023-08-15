0cc8e09416fd948aeb6e13c95595cf7f85646ac8
wxvirus <1418667580@qq.com>
2023-08-15 22:14:00 +0800

    feat: 登录页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,26 +2,30 @@
 </script>
 
 <template>
-  <h1>登录页面</h1>
-  <el-row span='8'></el-row>
-  <el-row span='8'>
-    <el-form>
-      <el-form-item>
-        <el-input placeholder='请输入账号' clearable></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input placeholder='请输入密码' show-password type='password' clearable></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type='primary'>登录
-          <svg-icon iconClass='eye'></svg-icon>
-          <svg-icon iconClass='eye-open'></svg-icon>
-        </el-button>
-      </el-form-item>
-    </el-form>
-  </el-row>
-  <el-row span='8'></el-row>
+  <div class='login-container'>
+    <div class='login-box'>
+      <!-- 左边背景图 start -->
+      <div class='login-left'>
+        <img src='@/assets/login/side-logo.png' alt=''>
+      </div>
+      <!-- 左边背景图 end -->
 
+      <!-- 登录表单 start -->
+      <div class='login-form'>
+        <!-- 标题和 logo start -->
+        <div class='login-title'>
+          <img src='@/assets/logo2.png' alt=''>
+        </div>
+        <!-- 标题和 logo end -->
+        <h2 class='title'>后台管理系统</h2>
+
+        <!-- 登录表单组件 start -->
+
+        <!-- 登录表单组件 end -->
+      </div>
+      <!-- 登录表单 end -->
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
